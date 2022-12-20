85ef70129a137d6d2cb5e124237c3deed90b986f
mohamad-ridwan <mr643062@gmail.com>
2022-12-20 10:39:27 +0700

    master 10:39 20/12/2022

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use(cors())
 dotenv.config()
 
 app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.urlencoded({ extended: true }))
 
 const navbarRoutes = require('./src/routes/navbar')
 const homeRoutes = require('./src/routes/home')
@@ -25,20 +25,20 @@ const zonaIntegritasRoutes = require('./src/routes/zonaintegritas')
 const layananPengaduanRoutes = require('./src/routes/layananpengaduan')
 const ppidRoutes = require('./src/routes/ppid')
 const kontakRoutes = require('./src/routes/kontak')
- 
+
 const fileStorage = multer.diskStorage({
-    destination: (req, file, cb)=>{
+    destination: (req, file, cb) => {
         cb(null, 'images')
     },
-    filename: (req, file, cb)=>{
+    filename: (req, file, cb) => {
         cb(null, `${new Date().getTime()}` + '-' + file.originalname)
     }
 })
 
 app.use('/images', express.static(path.join(__dirname, 'images')))
-app.use(multer({storage: fileStorage}).single('image'))
+app.use(multer({ storage: fileStorage }).single('image'))
 
-app.use((req, res, next)=>{
+app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*")
     res.header("Access-Control-Allow-Credential", "true")
     res.header("Access-Control-Allow-Headers", "Origin, Authorization, X-Requested-With, Content-Type, Accept")
@@ -57,26 +57,22 @@ app.use('/v8/layanan-pengaduan-masyarakat', layananPengaduanRoutes)
 app.use('/v9/ppid', ppidRoutes)
 app.use('/v10/kontak', kontakRoutes)
 
-app.use((error, req, res, next)=>{
+app.use((error, req, res, next) => {
     const status = error.errorStatus || 500
     const message = error.message
     const data = error.data
-    res.status(status).json({message: message, data: data})
+    res.status(status).json({ message: message, data: data })
 })
 
 const uri = process.env.MONGO_DB_URI
 
 const PORT = process.env.PORT || 6500
 
-mongoose.connect(uri, {useNewUrlParser: true, useUnifiedTopology: true})
-
-const connection = mongoose.connection
-
-connection.once('open', ()=>{
-    console.log("MongoDB database connection has been established successfully.")
-})
-
-// Listener
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`)
-})
+mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then((res) => {
+        // Listener
+        app.listen(PORT, () => {
+            console.log(`Server is running on port ${PORT}`)
+        })
+    })
+    .catch(err => console.log(err))
